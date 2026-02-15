207b9d475df0c4dd200cb8545b0720104064eaa5
Shyam Sundar Sahoo <shyamsundarsahoo2002@gmail.com>
2026-02-15 19:43:22 +0530

    first commit

---
+++ src/main/java/com/smartcart/SmartcartEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.smartcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcartEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcartEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcart/SmartcartEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcartEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
